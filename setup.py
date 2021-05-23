9330da53134c5a9d6a48e813ef57ac98e78db939
Jakub Święcicki <jaic@demant-technology.com>
2021-05-23 18:33:21 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+
+setup(
+    name="jaic_adding",
+    version="0.0.1",
+    # author="Jeff Hale",
+    # author_email="jeffmshale@gmail.com",
+    description="jaic adding package",
+    # long_description=readme,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/your_package/homepage/",
+    # packages=find_packages(),
+    # install_requires=requirements,
+    # classifiers=[
+    #     "Programming Language :: Python :: 3.7",
+    #     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    # ],
+)
+
